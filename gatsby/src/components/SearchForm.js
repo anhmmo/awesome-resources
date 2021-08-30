95f89d7173a6a07960f03e9e539276a1427e4bd6
anhmmo <aun@metropolia.fi>
2021-08-30 11:00:59 +0300

    create SearchForm section

---
+++ gatsby/src/components/SearchForm.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function SearchForm() {
+  return (
+    <form action="">
+      <input type="text" placeholder="Type sources name, topic or URL" />
+    </form>
+  )
+}
