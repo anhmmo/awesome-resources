c92312f098f8fdbcdc2bfa5cd0edbe1f86ad0cba
anhmmo <aun@metropolia.fi>
2021-08-30 11:01:36 +0300

    list item

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,22 @@
 import * as React from "react"
-import Layout from "../components/Layout"
+import ResourceList from "../components/ResourceList"
+import CategoryFilter from "../components/CategoryFilter"
+import SearchForm from "../components/SearchForm"
+import Pagination from "../components/Pagination"
+import SEO from "../components/SEO"
 
-const IndexPage = () => {
+export default function HomePage({ data, pageContext }) {
+  const resources = data.resources.nodes
+  console.log(resources)
   return (
-    <Layout>
+    <>
+      <SEO
+        title={
+          pageContext.category
+            ? `Resorces With ${pageContext.category}`
+            : `All Resorces`
+        }
+      />
       <section className="header">
         <div className="wrapper">
           <h1>Awere - Design resources for developers</h1>
@@ -12,355 +25,53 @@ const IndexPage = () => {
             <span className="hightlight">UI</span> resources from stock photos,
             web templates, CSS frameworks, UI libraries, tools and much more
           </h2>
-          <form action="">
-            <input type="text" placeholder="Type sources name, topic or URL" />
-          </form>
-          <ul className="list-topic">
-            <li>
-              {" "}
-              UI Graphics<span>3</span>
-            </li>
-            <li className="green">
-              {" "}
-              Fonts<span>3</span>
-            </li>
-            <li>
-              {" "}
-              Colors<span>3</span>
-            </li>
-            <li>
-              {" "}
-              Stock Music & Sound Effects<span>3</span>
-            </li>
-            <li className="yellow">
-              {" "}
-              Icons<span>98</span>
-            </li>
-            <li>
-              {" "}
-              Logos<span>177</span>
-            </li>
-            <li>
-              {" "}
-              Favicons<span>6</span>
-            </li>
-            <li>
-              {" "}
-              Icon Fonts<span>4</span>
-            </li>
-            <li className="pea">
-              {" "}
-              Stock Photos<span>33</span>
-            </li>
-            <li>
-              {" "}
-              Stock Videos<span>09</span>
-            </li>
-            <li className="lavender">
-              {" "}
-              Vectors & Clipart<span>37</span>
-            </li>
-            <li>
-              {" "}
-              Product & Image Mockups<span>1</span>
-            </li>
-            <li>
-              {" "}
-              HTML & CSS Templates<span>2</span>
-            </li>
-            <li className="aqua">
-              {" "}
-              CSS Frameworks<span>7</span>
-            </li>
-          </ul>
+          <SearchForm />
+          <CategoryFilter activeCategory={pageContext.category} />
         </div>
       </section>
-      <section className="category">
-        <div className="wrapper">
-          <div className="category-list flex">
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <img
-                  className="close-btn"
-                  src="https://i.imgur.com/4nDYTIT.png"
-                  alt=""
-                />
-
-                <div
-                  className="preview-window"
-                  title="click to view bigger image"
-                >
-                  <a target="_blank" href="https://i.imgur.com/k8i8X9v.png">
-                    {" "}
-                    <img src="https://i.imgur.com/k8i8X9v.png" alt="" />
-                  </a>
-                </div>
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://ian.umces.edu/apple-touch-icon.png"
-                      alt="test"
-                    />
-                  </div>
-                  <p title="Integration & Application Network Image Library">
-                    Integration & Application Network Image Library
-                  </p>
-                  <a href="#">
-                    {/*<div className="header-link"></div>*/}
-
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p>
-                    An online social networking community and creative research
-                    platform.
-                  </p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li>included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://freepik.cdnpk.net/img/favicons/apple-icon-144x144.png?v=2018082101"
-                      alt="test"
-                    />
-                  </div>
-                  <p>Freepik</p>
-                  <a href="#">
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p>
-                    Fully coded HTML templates to help you easily build your
-                    startup landing page without hassles.
-                  </p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li className="red">included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://illlustrations.co/icons/icon-48x48.png?v=1d0394bf9fc267e40ccc6d562175855a"
-                      alt="test"
-                    />
-                  </div>
-                  <p>Illlustrations</p>
-                  <a href="#">
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p
-                    title="Create stunning mockups using latest device frames like
-                    iPhone X & custom backgrounds that make your mobile or
-                    website design standout, right from your browser quickly &
-                    for free."
-                  >
-                    Create stunning mockups using latest device frames like
-                    iPhone X & custom backgrounds that make your mobile or
-                    website design standout, right from your browser quickly &
-                    for free.
-                  </p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li className="red">included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://smartmockups.com/apple-touch-icon.png"
-                      alt="test"
-                    />
-                  </div>
-                  <p>Smart Mockups</p>
-                  <a href="#">
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p>Create stunning product mockups (free & premium)</p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li className="red">included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://screenshot.rocks/apple-touch-icon.png"
-                      alt="test"
-                    />
-                  </div>
-                  <p>Screenshot.rocks</p>
-                  <a href="#">
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p>
-                    Create beautiful browser & mobile mockups in seconds
-                  </p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li className="red">included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="flex-item three category-item">
-              <div className="single-item">
-                <div className="item-header">
-                  <div className="image-box">
-                    <img
-                      src="https://html5up.net/assets/icons/apple-touch-icon-precomposed.png"
-                      alt="test"
-                    />
-                  </div>
-                  <p>HTML5Up</p>
-                  <a href="#">
-                    <div className="header-link"></div>
-                  </a>
-                </div>
-                <div className="item-body">
-                  {" "}
-                  <p>Very modern, unique responsive HTML5/CSS3 themes</p>{" "}
-                  <ul className="list-tag">
-                    <a href="#">
-                      {" "}
-                      <li className="green">free</li>
-                    </a>
-                    <a href="#">
-                      <li className="red">included paid services</li>
-                    </a>
-                  </ul>
-                </div>
-                <div className="item-footer">
-                  <div className="item-preview-link">
-                    <div className="item-preview-image">
-                      <img src="https://i.imgur.com/yrvSIVk.png" alt="test" />
-                    </div>
-                    <div className="preview-link">preview</div>
-                  </div>
-                  <a className="bookmark-link" href="#">
-                    like
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
+      <ResourceList resources={resources} />
+      <div className="wrapper">
+        <Pagination
+          pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+          totalCount={data.resources.totalCount}
+          currentPage={pageContext.currentPage || 1}
+          skip={pageContext.skip}
+          base=""
+        />
+      </div>
+    </>
   )
 }
 
-export default IndexPage
+export const query = graphql`
+  query ResourceQuery(
+    $categoryRegex: String
+    $skip: Int = 0
+    $pageSize: Int = 20
+  ) {
+    resources: allSanityResource(
+      filter: { category: { elemMatch: { _id: { eq: $categoryRegex } } } }
+      limit: $pageSize
+      skip: $skip
+    ) {
+      totalCount
+      nodes {
+        name
+        link
+        description
+        _id
+        slug {
+          current
+        }
+        category {
+          _id
+          name
+          desc
+          slug {
+            current
+          }
+        }
+      }
+    }
+  }
+`
