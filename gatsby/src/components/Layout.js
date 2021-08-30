fdece73f1617e363d44040434da65a01650a34c1
anhmmo <aun@metropolia.fi>
2021-08-30 10:42:31 +0300

    remove unused styled-component

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import styled from "styled-components"
 import "normalize.css"
 import GlobalStyles from "../styles/GlobalStyles"
 import Typography from "../styles/Typography"
