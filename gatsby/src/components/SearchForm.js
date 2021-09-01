6281187ae72c07113c0f52ad3ab9a40b074feb64
anhmmo <aun@metropolia.fi>
2021-09-01 21:47:05 +0300

    change placeholder

--- gatsby/src/components/SearchForm.js
+++ gatsby/src/components/SearchForm.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function SearchForm() {
   return (
     <form action="">
-      <input type="text" placeholder="Type sources name, topic or URL" />
+      <input type="text" placeholder="Type sources name, category or URL" />
     </form>
   )
 }
